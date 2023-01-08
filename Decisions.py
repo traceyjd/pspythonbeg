15b011aeeb520a7491ca8331c09045b697fb1f91
Tracey <tracey.tdoherty@gmail.com>
2023-01-08 19:47:48 +0000

    First commit

--- Decisions.py
+++ Decisions.py
@@ -8,18 +8,44 @@
 #  != NOT equal
 
 temperature = 95
-
 if temperature > 80:
     print("Its too hot!")  # Code Block
     print("Stay inside!")
 
 
 temperature = 75
-
 if temperature > 80:
     print("Its too hot!")  # Code Block
     print("Stay inside!")
 print("Have a good day!")
 
 
+#  Adding the else
+temperature = 75
+if temperature > 80:
+    print("Its too hot!")  # Code Block
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
+
+
+
+#  Adding the elif for less than 60
+temperature = 50
+if temperature > 80:
+    print("Its too hot!")  # Code Block
+    print("Stay inside!")
+elif temperature < 60:
+    print("Its too cold!")
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
+
+
+#  Adding the or statement
+temperature = 75
+if temperature > 80 or temperature < 60:
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
 
