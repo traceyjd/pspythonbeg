81ffcc8155b26a675bf57143b1f81f9a6abc78f7
Tracey <tracey.tdoherty@gmail.com>
2023-01-08 19:41:22 +0000

    First commit

---
+++ Decisions.py
@@ -0,0 +1,25 @@
+#  Weather
+#  6 Comparators in Python
+#  < less than
+#  <= less than equal to
+#  == equal to
+#  greater than equal to
+#  greater than
+#  != NOT equal
+
+temperature = 95
+
+if temperature > 80:
+    print("Its too hot!")  # Code Block
+    print("Stay inside!")
+
+
+temperature = 75
+
+if temperature > 80:
+    print("Its too hot!")  # Code Block
+    print("Stay inside!")
+print("Have a good day!")
+
+
+
