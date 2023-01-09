c0fe03fc3f01d1ad18663b75d370a4a76940108c
Tracey <tracey.tdoherty@gmail.com>
2023-01-09 21:11:17 +0000

    First commit

--- newinterestcal.py
+++ newinterestcal.py
@@ -10,13 +10,19 @@ months = int(input("How many months do you want to see results for?\n")) # 24
 # Divide apr by 100 to make it a percent, then divide by 12 to make monthly
 monthly_rate = apr/100/12
 
-# Add interest
-interest_paid = money_owed * monthly_rate
-money_owed = money_owed + interest_paid
+for i in range(months):
+    # Add interest
+    interest_paid = money_owed * monthly_rate
+    money_owed = money_owed + interest_paid
 
-# Make payment
-money_owed = money_owed - payment
+    if (money_owed - payment < 0):
+        print("The last payment is", money_owed)
+        print("You paid off the loan in", i+1, "months")
+        break
 
-# Print the results after this month
-print("Paid", payment, "of which", interest_paid, "was interest.", end=" ")
-print("Now I owe", money_owed)
+    # Make payment
+    money_owed = money_owed - payment
+
+    # Print the results after this month
+    print("Paid", payment, "of which", interest_paid, "was interest.", end=" ")
+    print("Now I owe", money_owed)
