dd5f9ce58b09f8b3b854267b4c36cd7131e6d12d
Tracey <tracey.tdoherty@gmail.com>
2023-01-09 19:30:59 +0000

    First commit

--- RPS.py
+++ RPS.py
@@ -7,9 +7,9 @@ if computer_choice == user_choice:
     print("TIE")
 elif user_choice == "rock" and computer_choice == "scissors":
     print("WIN")
-elif user_choice == paper and computer_choice == "rock":
+elif user_choice == "paper" and computer_choice == "rock":
     print("WIN")
-elif user_choice == "scissors" and computer_choice == "paper"
+elif user_choice == "scissors" and computer_choice == "paper":
     print("WIN")
 else:
     print("You lose :( Computer wins :D")
