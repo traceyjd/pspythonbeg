d603610ddf3fe02d62a06d4de3cc9756980ee127
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 09:37:39 +0000

    First commit

--- Dictionary.py
+++ Dictionary.py
@@ -1,2 +1,19 @@
 
 
+acronyms = {}
+
+acronyms["LOL"] = "Laugh out loud" # Adding in values to our empty list
+acronyms["IDK"] = "I don't know"
+acronyms["TBH"] = "To be honest"
+
+# Adding items
+acronyms["TBH"]  = "honestly"# A value is updated the same way a value is added
+
+print(acronyms)
+
+#  Notice that our 3 key-value pairs are there but order is random in a dictionary
+
+# Removing items - remove items by looking up the key
+
+
+
--- current.py
+++ current.py
@@ -6,5 +6,8 @@ acronyms["LOL"] = "Laugh out loud"
 acronyms["IDK"] = "I don't know"
 acronyms["TBH"] = "To be honest"
 
+acronyms["TBH"]  = "honestly"# A value is updated the same way a value is added
+
 print(acronyms)
 
+#  Notice that our 3 key-value pairs are there but order is random in a dictionary
