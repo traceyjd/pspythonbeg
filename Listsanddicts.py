aa471b6801f32e9677bf260dc1d586a7593fd831
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 11:09:12 +0000

    First commit

---
+++ Listsanddicts.py
@@ -0,0 +1,8 @@
+
+#  Breakfast lunch and dinner
+
+# In python you can have a container of contrainers - menus is a list of lists.
+
+menus = [ ["Egg Sandwich", "Bagel", "Coffee"],
+          ["BLT", "PB&J", "Turkey SAndwich"],
+          ]
