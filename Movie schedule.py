2cfed8775da37d53482f806effd0b97d32877899
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 11:06:09 +0000

    First commit

--- Movie schedule.py
+++ Movie schedule.py
@@ -11,3 +11,9 @@ print("We're showing the following movies:")
 for key in current_movies:
     print(key)
 movie = input("What movie would you like the showtime for?\n")
+
+showtime = current_movies.get(movie)
+if showtime == None:
+    print("Requested showtime isn't playing")
+else:
+    print(movie, "is playing at", showtime)
