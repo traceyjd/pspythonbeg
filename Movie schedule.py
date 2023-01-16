1cf2204889b04d202a3f9a4ad7a52f959d791973
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 11:01:51 +0000

    First commit

---
+++ Movie schedule.py
@@ -0,0 +1,13 @@
+
+
+# Movie Schedule
+
+current_movies = {"The Gringe":"11:00am",
+                  "Rudolph":"1:00pm",
+                  "Fronsty the Snowman":"3:00pm",
+                  "Christmas Vacation":"5:00pm"}
+
+print("We're showing the following movies:")
+for key in current_movies:
+    print(key)
+movie = input("What movie would you like the showtime for?\n")
