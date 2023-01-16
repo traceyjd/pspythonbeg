56d6f27671c4e5a4e6722828f923812abcb8c424
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 16:46:52 +0000

    First commit

--- weather.py
+++ weather.py
@@ -4,8 +4,8 @@
 import requests
 
 api_key = "fb1a5654dbeda7dacb0c4ca2077ae634"
-city = "Edinburgh"
-url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
+city = "Livingston"
+url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
 request = requests.get(url)
 json = request.json()
--- weather1.py
+++ weather1.py
@@ -2,7 +2,7 @@
 import requests
 api_key = "fb1a5654dbeda7dacb0c4ca2077ae634"
 city = "Edinburgh"
-url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
+url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
 request = requests.get(url)
 json = request.json()
