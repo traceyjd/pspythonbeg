5dd1ffc84a953c2df16a686b1192312ff62488fa
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 10:40:47 +0000

    First commit

--- Dictionary.py
+++ Dictionary.py
@@ -34,3 +34,20 @@ acronyms["TBH"] = "To be honest"
 
 definition = acronyms.get("BTW") # Using get wont crach your program
 print(definition)
+
+# None
+
+# None is returned to show the absence of a value and also evaluates to false in a conditional
+
+acronyms = {}
+
+acronyms["LOL"] = "Laugh out loud" # Adding in values to our empty list
+acronyms["IDK"] = "I don't know"
+acronyms["TBH"] = "To be honest"
+
+definition = acronyms.get("BTW") # Using get wont crach your program
+
+if definition:
+    print(definition)
+else:
+    print("Key does not exisit")
