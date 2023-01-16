ef36d7977855effcbb29cb031be2105d3fe83ebc
Tracey <tracey.tdoherty@gmail.com>
2023-01-16 15:06:39 +0000

    First commit

--- current.py
+++ current.py
@@ -1,13 +1,9 @@
 
 
-acronyms = {}
+import requests
 
-acronyms["LOL"] = "Laugh out loud"
-acronyms["IDK"] = "I don't know"
-acronyms["TBH"] = "To be honest"
+response = requests.get("http://api.open-notify.org/astros.json")
+json = response.json()
 
-acronyms["TBH"]  = "honestly"# A value is updated the same way a value is added
-
-print(acronyms)
-
-#  Notice that our 3 key-value pairs are there but order is random in a dictionary
+for person in json["people"]:
+    print(person)
--- space_people.py
+++ space_people.py
@@ -21,5 +21,22 @@
 #
 # JSON stands for JavaScript Object Notation
 
+import requests
+
+response = requests.get("http://api.open-notify.org/astros.json")
+json = response.json()
+print(json)
+
+# To access just the persons name
+
+import requests
+
+response = requests.get("http://api.open-notify.org/astros.json")
+json = response.json()
+
+print("The people currently in space are:")
+for person in json["people"]:
+    print(person["name"])
+
 
 
