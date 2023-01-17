1b6491e671986f6d70c407978085c7bc98dfa1dc
Tracey <tracey.tdoherty@gmail.com>
2023-01-17 15:21:04 +0000

    First commit

--- dice_roll.py
+++ dice_roll.py
@@ -4,12 +4,22 @@ def roll_dice():
     dice_total = random.randint(1,6) + random.randint(1,6)
     return dice_total
 
-player1 = input("Enter player 1's nme: ")
-player2 = input("Enter player 2's nme: ")
+def main():
+    player1 = input("Enter player 1's name: ")
+    player2 = input("Enter player 2's name: ")
 
-roll1 = roll_dice()
-roll2 = roll_dice()
+    roll1 = roll_dice()
+    roll2 = roll_dice()
 
-print(player1, "rolled", roll1)
-print(player2, "rolled", roll2)
+    print(player1, "rolled", roll1)
+    print(player2, "rolled", roll2)
+
+    if roll1 > roll2:
+        print(player1, "WINS!")
+    elif roll2 > roll1:
+        print(player2, "WINS!")
+    else:
+        print("You Tie!")
+
+main()
 
