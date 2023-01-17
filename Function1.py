6ae9c40b5e3cbee204fb2d1f693d5a0159d9722b
Tracey <tracey.tdoherty@gmail.com>
2023-01-17 10:56:30 +0000

    First commit

---
+++ Function1.py
@@ -0,0 +1,13 @@
+
+def greeting(name):             #  The function needs to be defined first
+    print("Hello", name)
+
+# Main Program
+input_name = input("Enter your name:\n")    # The 1st line of code that isnt in a definition is where the program starts.
+# Enter your name is displayed and the input is saved in input_name variable
+
+greeting(input_name)             # Before the function is called # The next line is then run which is a call to the
+# greeting () function and the value of input_name is passed in which is trish
+# Since we call the function greeting() we now go to the function and the value of input_name is passed in. Then we
+# go to the next line print and the name that was passed in by input_name to name is printed to the screen
+
